610d35d2cc12b12210a32a52017d234bef3d8906
Ann Meng <anmeng@microsoft.com>
2019-05-20 16:22:32 +0800

    revert change

--- src/java2yaml/Steps/CentralizeDocument.cs
+++ src/java2yaml/Steps/CentralizeDocument.cs
@@ -1,27 +1,19 @@
 ﻿namespace Microsoft.Content.Build.Java2Yaml
 {
-    using System.IO;
     using System.Threading.Tasks;
 
     public class CentralizeDocument : IStep
     {
-        public string StepName => "CentralizeDocument";
+        public string StepName
+        {
+            get { return "CentralizeDocument"; }
+        }
 
         public Task RunAsync(ConfigModel config)
         {
             return Task.Run(() =>
             {
-                var exists = System.IO.Directory.Exists(config.OutputPath);
-
-                if (!exists)
-                {
-                    System.IO.Directory.CreateDirectory(config.OutputPath);
-                }
-
-                foreach (var r in config.RepositoryFolders)
-                {
-                    CopyProcesser.ExcludeCopy(Path.Combine(r, "_javadoc"), config.OutputPath);
-                }
+                //TODO
             }
             );
         }
