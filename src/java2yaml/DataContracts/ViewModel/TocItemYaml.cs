26bcca5f9886bfa6e162b78a0e7ff63aad023042
Ann Meng <anmeng@microsoft.com>
2019-09-12 15:06:48 +0800

    implement merge toc for multiple packages scenario

---
+++ src/java2yaml/DataContracts/ViewModel/TocItemYaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Microsoft.Content.Build.Java2Yaml
+{
+    using System;
+
+    using YamlDotNet.Serialization;
+
+    [Serializable]
+    public class TocItemYaml
+    {
+        [YamlMember(Alias = "uid")]
+        public string Uid { get; set; }
+
+        [YamlMember(Alias = "name")]
+        public string Name { get; set; }
+
+        [YamlMember(Alias = "href")]
+        public string Href { get; set; }
+
+        [YamlMember(Alias = "items")]
+        public TocYaml Items { get; set; }
+    }
+}
--- src/java2yaml/Steps/GenerateToc.cs
+++ src/java2yaml/Steps/GenerateToc.cs
@@ -1,12 +1,17 @@
 ﻿namespace Microsoft.Content.Build.Java2Yaml
 {
-    using System;
+
+    using System.Collections.Generic;
     using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
+    using Microsoft.DocAsCode.YamlSerialization;
+    using YamlDotNet.Serialization;
 
     public class GenerateToc : IStep
     {
+        private static ConfigModel _config;
+
         public string StepName
         {
             get { return "GenerateToc"; }
@@ -16,7 +21,9 @@
         {
             return Task.Run(() =>
             {
-                if (config.RepositoryFolders.Count == 1)
+                _config = config;
+
+                if (_config.RepositoryFolders.Count == 1)
                 {
                     var sourcePath = Path.Combine(config.RepositoryFolders[0], Constants.Doc);
 
@@ -32,6 +39,13 @@
                 else
                 {
                     MergeToc();
+
+                    ConsoleLogger.WriteLine(new LogEntry
+                    {
+                        Phase = StepName,
+                        Level = LogLevel.Info,
+                        Message = $" Merge completed, write toc.yml to {_config.OutputPath} ..."
+                    });
                 }
             }
             );
@@ -39,7 +53,45 @@
 
         private void MergeToc()
         {
-            throw new NotImplementedException();
+            var tocFiles = new List<TocYaml>();
+
+            tocFiles = GetTocList()
+                .Select(file => DeserializeYaml(file))
+                .ToList();
+
+            WriteMergedTocToDisk(tocFiles);
+        }
+
+        private List<string> GetTocList()
+        {
+            return ( _config.RepositoryFolders
+                    .Select(path => FileUtility.GetFilesByName(Path.Combine(path, Constants.Doc), Constants.Toc).FirstOrDefault())
+                    .Select(x => x.FullName.ToString())
+                    .ToList()
+            );
+        }
+
+        private TocYaml DeserializeYaml(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                var toc = new Deserializer().Deserialize<TocYaml>(reader);
+                return toc;
+            }
+        }
+
+        private void WriteMergedTocToDisk(List<TocYaml> tocFiles)
+        {
+            string tocFile = Path.Combine(_config.OutputPath, Constants.Toc);
+
+            using (var writer = new StreamWriter(tocFile))
+            {
+                writer.WriteLine(Constants.YamlMime.TableOfContent);
+
+                var yamlSerializer = new YamlSerializer();
+
+                tocFiles.ForEach(tocYaml => yamlSerializer.Serialize(writer, tocYaml));
+            }
         }
     }
 }
