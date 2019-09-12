c121c645cffeb392cd209e89279c30bf41dea351
anmeng10101 <33647870+anmeng10101@users.noreply.github.com>
2019-09-12 15:39:47 +0800

    implement merge toc for multiple packages scenario (#8)
    * implement merge toc for multiple packages scenario
    
    * resolve comments

---
+++ src/java2yaml/DataContracts/ConfigModel/PackageBasedConfigModel.cs
@@ -0,0 +1,31 @@
+﻿namespace Microsoft.Content.Build.Java2Yaml
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+
+    [Serializable]
+    public class PackageBasedConfigModel
+    {
+        [JsonProperty(Constants.OutputPath, Required = Required.DisallowNull)]
+        public string OutputPath { get; set; }
+
+        [JsonProperty(Constants.Language)]
+        public string Language { get; set; } = "java";
+
+        public List<Package> Packages { get; set; }
+    }
+
+    public class Package
+    {
+        [JsonProperty(Constants.ArtifactId, Required = Required.DisallowNull)]
+        public string ArtifactId { get; set; }
+
+        [JsonProperty(Constants.InputPaths)]
+        public List<string> InputPaths { get; set; }
+
+        [JsonProperty(Constants.ExcludePaths)]
+        public List<string> ExcludePaths { get; set; }
+    }
+}
---
+++ src/java2yaml/DataContracts/ViewModel/TocYaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.Content.Build.Java2Yaml
+{
+    using System;
+
+    using System.Collections.Generic;
+
+    [Serializable]
+    public class TocYaml
+        : List<TocItemYaml>
+    {
+        public TocYaml(IEnumerable<TocItemYaml> items) : base(items) { }
+        public TocYaml() : base() { }
+    }
+}
